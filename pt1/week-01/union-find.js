5ab0196d5014b9b24d1ffe67715d4434bf329921
Silva404 <brenosilva0252@gmail.com>
2020-07-10 09:23:07 -0300

    init

---
+++ pt1/week-01/union-find.js
@@ -0,0 +1,20 @@
+function unionFind(strings) {
+
+}
+
+class UF {
+    constructor(N) {
+        // union ?
+
+        while (!union == null) {
+            this.p = Number(p)
+            this.q = Number(q)
+            if (!union.connected(p, q)) {
+
+            }
+        }
+    }
+}
+
+
+const test = ['{0}, {1,4,5}, {2,3,6,7}']
